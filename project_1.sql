2cf7578aa5896b738345cabe1b0a32f0056e1919
Audrey Louie <98408104+audreylouie@users.noreply.github.com>
2025-10-07 18:33:15 -0400

    Create project_1.sql

---
+++ project_1.sql
@@ -0,0 +1,39 @@
+CREATE TABLE friends(
+  id INTEGER,
+  name TEXT,
+  birthday DATE
+);
+
+INSERT INTO friends (id, name, birthday)
+VALUES (1,'Ororo Munroe', '1940-05-30');
+
+INSERT INTO friends (id, name, birthday)
+VALUES (2, 'Jane Smith', '1982-09-20');
+
+INSERT INTO friends (id, name, birthday)
+VALUES (3, 'William Jones', '2000-03-29');
+
+UPDATE friends
+SET name = 'Storm'
+WHERE id = 1;
+
+ALTER TABLE friends
+ADD COLUMN email TEXT;
+
+UPDATE friends
+SET email = 'storm@codecademy.com'
+WHERE id = 1;
+
+UPDATE friends
+SET email = 'janesmith@codecademy.com'
+WHERE id = 2;
+
+UPDATE friends
+SET email = 'williamjones@codecademy.com'
+WHERE id = 3;
+
+DELETE FROM friends
+WHERE id = 1;
+
+SELECT *
+FROM friends;
